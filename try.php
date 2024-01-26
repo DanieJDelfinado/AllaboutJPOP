b85242343519b9fa73850c3f39c21c7c20da73f6
DanieJDelfinado <djdelfinado@gmail.com>
2024-01-26 20:06:50 +0800

    s

---
+++ try.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+     
+     <h1>You are my Special</h1>
+  <?php
+  echo "My first PHP script!<br>";
+   
+
+  echo "\nIm not crazy Im just a little unwell\n";
+  
+    ?>
+ </body>
+ </html>
